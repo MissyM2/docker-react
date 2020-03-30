a5bfefa349588c33a9e4960fdd16fc3e59ea937b
Missy Maloney <mmaloney@healthstarInfo.com>
2020-03-30 15:21:40 -0400

    changed app test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Getting hungry!</p>
+        <p>I am super hungry. Gimme licorice!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
